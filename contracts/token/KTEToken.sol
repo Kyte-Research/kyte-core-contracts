665219b4d1f7eaa0fe8da40a026c6202838e94a7
surya singh <waytoheaven.mit1@gmail.com>
2022-01-17 00:49:04 +0000

    Add token contract

---
+++ contracts/token/KTEToken.sol
@@ -0,0 +1,84 @@
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+
+contract KTEToken is
+  ERC20,
+  ERC20Capped,
+  ERC20Burnable,
+  ERC20Pausable,
+  AccessControl,
+  ERC20Permit,
+  ERC20Votes
+{
+  bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+  bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+
+  constructor(
+    string memory _name,
+    string memory _symbol,
+    uint256 _totalSupply,
+    address _minter,
+    address _pauser
+  ) ERC20(_name, _symbol) ERC20Capped(_totalSupply) ERC20Permit(_name) {
+    _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+    _setupRole(MINTER_ROLE, _minter);
+    _setupRole(PAUSER_ROLE, _pauser);
+  }
+
+  function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+    _mint(to, amount);
+  }
+
+  function _mint(address account, uint256 amount)
+    internal
+    virtual
+    override(ERC20, ERC20Capped, ERC20Votes)
+  {
+    super._mint(account, amount);
+  }
+
+  function _burn(address account, uint256 amount)
+    internal
+    override(ERC20, ERC20Votes)
+  {
+    super._burn(account, amount);
+  }
+
+  /**
+   * @dev Pauses all token transfers.
+   */
+  function pause() public onlyRole(PAUSER_ROLE) {
+    _pause();
+  }
+
+  /**
+   * @dev Unpauses all token transfers.
+   */
+  function unpause() public onlyRole(PAUSER_ROLE) {
+    _unpause();
+  }
+
+  function _beforeTokenTransfer(
+    address from,
+    address to,
+    uint256 amount
+  ) internal virtual override(ERC20, ERC20Pausable) {
+    super._beforeTokenTransfer(from, to, amount);
+  }
+
+  function _afterTokenTransfer(
+    address from,
+    address to,
+    uint256 amount
+  ) internal override(ERC20, ERC20Votes) {
+    super._afterTokenTransfer(from, to, amount);
+  }
+}
