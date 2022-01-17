83051a72952eb73e38e5173e823541f179da8240
surya singh <waytoheaven.mit1@gmail.com>
2022-01-17 00:55:01 +0000

    Add token vesting contract

---
+++ contracts/vesting/ITokenVesting.sol
@@ -0,0 +1,30 @@
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+import "./ITokenVestingEvent.sol";
+
+interface ITokenVesting is ITokenVestingEvent  {
+   struct VestingSchedule {
+        bool initialized;
+        // beneficiary of tokens after they are released
+        address beneficiary;
+        // cliff period in seconds
+        uint256 cliff;
+        // start time of the vesting period
+        uint256 start;
+        // duration of the vesting period in seconds
+        uint256 duration;
+        // duration of a slice period for the vesting in seconds
+        uint256 slicePeriodSeconds;
+        // whether or not the vesting is revocable
+        bool revocable;
+        // Total number of tokens in vested
+        uint256 amountTotal;
+        // amount of tokens released
+        uint256 released;
+        // whether or not the vesting has been revoked
+        bool revoked;
+        // amount of token released upfront
+        uint256 upFront;
+    }
+}
---
+++ contracts/vesting/ITokenVestingEvent.sol
@@ -0,0 +1,24 @@
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+interface ITokenVestingEvent {
+    event Released(uint256 amount);
+    event Revoked();
+
+    event AddVestingSchedule(
+        bytes32 vestingScheduleId,
+        address beneficiary,
+        uint256 cliff,
+        uint256 start,
+        uint256 duration,
+        uint256 slicePeriodSeconds,
+        bool revocable,
+        uint256 amountTotal,
+        uint256 released,
+        bool revoked,
+        uint256 upFront
+    );
+    event RevokeVestingShedule(bytes32 vestingScheduleId);
+    event UpfrontTokenTransfer(bytes32 vestingScheduleId, address beneficiary, uint256 amount);
+    event ReleaseVestedToken(bytes32 vestingScheduleId, address beneficiary, uint256 amountReleased);
+}
