2c9edd5afb57201a812a418f1a3930daba8c714e
surya singh <waytoheaven.mit1@gmail.com>
2022-01-17 00:52:17 +0000

    Add multisig wallet contract

---
+++ contracts/multisig/IMultiSigWalletEvents.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+interface IMultiSigWalletEvents {
+    event Deposit(address indexed sender, uint amount, uint balance);
+    event SubmitTransaction(
+        address indexed owner,
+        uint indexed txIndex,
+        address indexed to,
+        uint value,
+        bytes data
+    );
+    event ConfirmTransaction(address indexed owner, uint indexed txIndex);
+    event RevokeConfirmation(address indexed owner, uint indexed txIndex);
+    event ExecuteTransaction(address indexed owner, uint indexed txIndex);
+}
