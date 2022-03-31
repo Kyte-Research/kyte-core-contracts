e7d44cebe392459d6a5720e51d635e3e8ce6594b
Muiz Nadeem <muiz.nadeem@gmail.com>
2022-03-31 23:03:55 +0530

    vesting tasks

--- tasks/index.ts
+++ tasks/index.ts
@@ -1,2 +1,3 @@
 import "hardhat-contract-sizer";
 import "./token";
+import "./vesting";
---
+++ tasks/vesting.ts
@@ -0,0 +1,53 @@
+import { task } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
+
+task("create-vesting", "Create token vesting")
+  .addParam("tokenAddress", "Token address")
+  .addParam("vestingAddress", "Vesting address")
+  .addParam("address", "Beneficiary address")
+  .addOptionalParam("tokens", "Number of tokens")
+  .setAction(async (args, hre) => {
+    const {
+      tokenAddress,
+      vestingAddress,
+      address,
+      tokens = "500",
+    } = args;
+    const accounts = await hre.ethers.getSigners();
+    const signer = accounts[0];
+
+    const TokenVesting = await hre.ethers.getContractFactory("TokenVesting");
+    const tokenVesting = new hre.ethers.Contract(
+      vestingAddress,
+      TokenVesting.interface,
+      signer
+    );
+    
+
+    // epoch current time
+    const baseTime = Number((new Date().getTime()/1000).toFixed(0));
+    const startTime = baseTime;
+    const cliff = 0;
+    const duration = 1000;
+    const slicePeriodSeconds = 1;
+    const revokable = true;
+    const amount = hre.ethers.utils.parseUnits(tokens, 18);
+    const upfront = hre.ethers.utils.parseUnits('0', 18);
+
+
+    await tokenVesting.createVestingSchedule(
+      address,
+      startTime,
+      cliff,
+      duration,
+      slicePeriodSeconds,
+      revokable,
+      amount,
+      upfront,
+      {
+        gasLimit: 8000000,
+      }
+    );
+
+  });
+
