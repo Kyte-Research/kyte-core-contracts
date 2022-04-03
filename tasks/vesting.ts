2efa44eb04eb573789da4fa9280c3972d66a8063
Rachit Magon <rachit.magon@gmail.com>
2022-04-03 22:44:04 +0530

    add release task

--- tasks/vesting.ts
+++ tasks/vesting.ts
@@ -7,12 +7,7 @@ task("create-vesting", "Create token vesting")
   .addParam("address", "Beneficiary address")
   .addOptionalParam("tokens", "Number of tokens")
   .setAction(async (args, hre) => {
-    const {
-      tokenAddress,
-      vestingAddress,
-      address,
-      tokens = "500",
-    } = args;
+    const { tokenAddress, vestingAddress, address, tokens = "500" } = args;
     const accounts = await hre.ethers.getSigners();
     const signer = accounts[0];
 
@@ -22,18 +17,16 @@ task("create-vesting", "Create token vesting")
       TokenVesting.interface,
       signer
     );
-    
 
     // epoch current time
-    const baseTime = Number((new Date().getTime()/1000).toFixed(0));
+    const baseTime = Number((new Date().getTime() / 1000).toFixed(0));
     const startTime = baseTime;
     const cliff = 0;
     const duration = 1000;
     const slicePeriodSeconds = 1;
     const revokable = true;
     const amount = hre.ethers.utils.parseUnits(tokens, 18);
-    const upfront = hre.ethers.utils.parseUnits('0', 18);
-
+    const upfront = hre.ethers.utils.parseUnits("1", 18);
 
     await tokenVesting.createVestingSchedule(
       address,
@@ -48,6 +41,25 @@ task("create-vesting", "Create token vesting")
         gasLimit: 8000000,
       }
     );
-
   });
 
+task("release", "Release vested tokens")
+  .addParam("vestingAddress", "Vesting address")
+  .addParam("vestingScheduleId", "Vesting Schedule Id")
+  .addParam("amount", "Number of tokens")
+  .setAction(async (args, hre) => {
+    const { amount, vestingAddress, vestingScheduleId } = args;
+    const accounts = await hre.ethers.getSigners();
+    const signer = accounts[0];
+
+    const TokenVesting = await hre.ethers.getContractFactory("TokenVesting");
+    const tokenVesting = new hre.ethers.Contract(
+      vestingAddress,
+      TokenVesting.interface,
+      signer
+    );
+
+    await tokenVesting.release(vestingScheduleId, amount, {
+      gasLimit: 8000000,
+    });
+  });
